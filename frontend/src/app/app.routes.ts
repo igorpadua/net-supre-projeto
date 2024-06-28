dcdd3fe77d0a4af990670afd02ca8b5a1e1e823f
igorpadua <igorpadua@tutanota.com>
2024-06-28 16:15:27 -0300

    feat: Criado o component de tela de inicio e criado a rota

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {TelaInicioComponent} from "./tela-inicio/tela-inicio.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TelaInicioComponent,
+  }
+];
---
+++ frontend/src/app/tela-inicio/tela-inicio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-inicio',
+  standalone: true,
+  imports: [],
+  templateUrl: './tela-inicio.component.html',
+  styleUrl: './tela-inicio.component.css'
+})
+export class TelaInicioComponent {
+
+}

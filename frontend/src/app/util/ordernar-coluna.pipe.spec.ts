0711e70de102e90b8042d1ed473a5cba6a0dee3d
igorpadua <igorpadua@tutanota.com>
2024-06-28 21:08:06 -0300

    feat: Criado piper para ordernação de colunas

---
+++ frontend/src/app/util/ordernar-coluna.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { OrdernarColunaPipe } from './ordernar-coluna.pipe';
+
+describe('OrdernarColunaPipe', () => {
+  it('create an instance', () => {
+    const pipe = new OrdernarColunaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/util/ordernar-coluna.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { orderBy } from 'lodash';
+
+@Pipe({
+  name: 'ordernarColuna',
+  standalone: true
+})
+export class OrdernarColunaPipe implements PipeTransform {
+
+  transform(lista: any[], colunaNome: string, ordenar: 'asc' | 'desc'): any[] {
+    return orderBy(lista, [colunaNome], [ordenar]);
+  }
+}
